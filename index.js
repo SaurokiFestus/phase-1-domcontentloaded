1a7760443eb438c2f594deea45a6143efd068dfb
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-06-14 09:19:29 +0300

    commit tree f567a2f6362ec6487a2621997954c1ead7941007

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("The DOM has loaded");
+//     modifyText()
+//   });
+// //   console.log("The DOM has not loaded")
+
+// function modifyText(){
+//     const text = document.getElementById("text")
+//     text.textContent = "This is really cool!"
+// }
